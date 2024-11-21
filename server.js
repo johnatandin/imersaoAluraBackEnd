cccc07c52b56102a7a4be1decc0dc2750dae46c2
johnatandin <johnatansr@hotmail.com>
2024-11-21 16:04:31 -0300

    refatorando com  nova arquitetura

--- server.js
+++ server.js
@@ -1,36 +1,10 @@
 import express from "express";
-import conectarAoBanco from "./src/config/dbConfig.js";
-
-const conexao = await conectarAoBanco(process.env.STRING_CONEXAO)
-
+import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
-app.use(express.json());
 
-//const posts = [
-  
- // { id: 1, descricao: "Uma foto teste", imagem: "https://placecats.com/millie/300/150" },
-  //{ id: 2, descricao: "Gato fazendo yoga", imagem: "https://placecats.com/millie/300/150" },
-  //{ id: 3, descricao: "Gato fazendo panqueca", imagem: "https://placecats.com/millie/300/150"},
-//];
+routes(app);
 
-// function BuscarPostPorID(id){
-//   return posts.findIndex((post)=>{
-//     return post.id === Number(id);
-
-//   })
-// }
-
-app.get("/posts",async (req, res) => {
-  const posts=  await getTodosPosts()
-  res.json(posts);
+app.listen(3000, () => {
+    console.log("Servidor escutando...");
 });
-
-
-
-async function  getTodosPosts(){
-  const db = conexao.db("imersao-alura-backend")
-  const colecao = db.collection("posts")
-  return colecao.find().toArray()
-
-}
